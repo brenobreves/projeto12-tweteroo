9ee96273a12e772f4a966b8520960e7d81b8fae8
breno.breves <brenocsb@dcc.ufrj.br>
2023-06-25 16:02:09 -0300

    Get tweets 100% funcional

--- src/app.js
+++ src/app.js
@@ -9,5 +9,27 @@ const PORT = 5000;
 const users = [];
 const tweets = [];
 
+// Configura uma função pra ser executada quando bater um GET na rota "/"
+app.get("/tweets", (req, res) => {
+    const sendObj = [];;
+    let avatar = "";
+
+    for(let i = 0 ; i < 10 ; i++){
+        if(tweets.length - i - 1 < 0){
+            break;
+        }
+        let uname = tweets[tweets.length - 1 - i].username;
+        let tweetUser = users.find( (user) => user.username === uname); 
+        sendObj.push(
+            {
+                username: tweetUser.username ,
+                tweet: tweets[tweets.length - i - 1].tweet ,
+                avatar: tweetUser.avatar
+            }
+        )
+    }
+    res.send(sendObj);
+});
+
 app.listen(PORT , () => console.log(`App rodando na porta ${PORT}`));
 
